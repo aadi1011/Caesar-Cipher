f3ddbf5657d1b287b68e9f04b931abcb38321e0f
Aadith S <62766172+ProxyHydra@users.noreply.github.com>
2021-07-27 19:03:53 +0530

    Created function for user choice to encrypt/decrypt.

--- Caesar Cipher/Caesar Cipher In Class.cpp
+++ Caesar Cipher/Caesar Cipher In Class.cpp
@@ -6,7 +6,8 @@ class Cipher
 {
 public:
 	void getInput();
-
+	void UserChoice();
+	
 private:
 	char message[1000];
 	int key;
@@ -15,7 +16,7 @@ private:
 
 void Cipher::getInput()
 {
-	cout << "Enter message to encrypt/decrypt\n (place two spaces before typing sentence the first time)\n\n--> ";
+	cout << "Enter message to encrypt/decrypt\n(place two spaces before typing sentence the first time)\n\n--> ";
 	cin.ignore(2, '\n');									//without this first 2 input char gets ommited. Reason:unknown
 	cin.getline(message, 1000);
 	system("cls");
@@ -26,8 +27,31 @@ void Cipher::getInput()
 	cout << "Message: " << message << "\nkey: " << key << endl;
 }
 
+void Cipher::UserChoice()
+{
+	
+	system("cls");
+	cout << "\nEnter your choice: \n1. Encrypt\n2. Decrypt\n\n--> ";
+	cin >> choice;
+	if (choice == 1)
+	{
+
+	}
+	else if (choice == 2)
+	{
+
+	}
+	else
+	{
+		cout << "Invalid input entered. Try again using '1' or '2'\n\n";
+		system("pause");
+		Cipher::UserChoice();
+	}
+}
+
 void main()
 {
 	Cipher C;
 	C.getInput();
+	C.UserChoice();
 }
